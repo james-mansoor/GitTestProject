90e08f7e2c5995f59638b9d7e6c80ef4be75efef
Tim Phillips <tim.phillips@covan.com>
2022-03-10 13:48:30 -0600

    Changed Name

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace GitTestProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Tim!");
+            Console.WriteLine("Hello James!");
         }
     }
 }
